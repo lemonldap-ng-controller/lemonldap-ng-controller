a331f74d4b3b2defe822756a98523ad869cb4ac6
Mathieu Parent <math.parent@gmail.com>
2018-01-18 07:53:56 +0100

    Add sync-files.sh, for files copied from other projects

---
+++ hack/sync-files.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright 2018 Mathieu Parent <math.parent@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GOPATH=$(go env GOPATH)
+
+if [ "$(pwd)" != "$GOPATH/src/github.com/lemonldap-ng-controller/lemonldap-ng-controller" ]; then
+  echo "Working dir not in GOPATH. Should be '$GOPATH/src/github.com/lemonldap-ng-controller/lemonldap-ng-controller'" >&2
+  exit 1
+fi
+
+for entry in \
+  'k8s.io/sample-controller/LICENSE .' \
+  'k8s.io/sample-controller/pkg/signals/*.go pkg/signals'
+do
+  src="$GOPATH/src/$(echo $entry | awk '{print $1}')"
+  dst_dir="$(echo $entry | awk '{print $2}')"
+  cp $src $dst_dir/
+done
+
+# ex: ts=2 sw=2 et filetype=sh
