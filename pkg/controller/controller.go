01ec8e604c98ff6668eb65ada3d3a5b5045897b8
Mathieu Parent <math.parent@gmail.com>
2018-01-18 09:25:44 +0100

    Fix golint: exported function NewIngressController should have comment or be unexported

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -56,6 +56,7 @@ func (c *IngressController) Run(stopCh <-chan struct{}) error {
 	return nil
 }
 
+// NewIngressController returns a new ingress controller
 func NewIngressController(kclient *kubernetes.Clientset, namespace string) *IngressController {
 	ingressWatcher := &IngressController{}
 
