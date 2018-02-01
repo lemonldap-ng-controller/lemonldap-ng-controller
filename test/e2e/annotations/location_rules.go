9c009002e2e58099e99ec39e548230713e88f8a5
Mathieu Parent <math.parent@gmail.com>
2018-02-01 13:09:23 +0100

    Add basic e2e tests

---
+++ test/e2e/annotations/location_rules.go
@@ -0,0 +1,106 @@
+/*
+Copyright 2018 Mathieu Parent <math.parent@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package annotations
+
+import (
+	"encoding/base64"
+	"net/http"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/parnurzeal/gorequest"
+
+	"github.com/lemonldap-ng-controller/lemonldap-ng-controller/test/e2e/framework"
+)
+
+var _ = framework.IngressNginxDescribe("Annotations - location-rules", func() {
+	f := framework.NewDefaultFramework("location-rules")
+
+	BeforeEach(func() {
+		err := f.NewEchoDeploymentWithReplicas(2)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+	})
+
+	It("should redirect to LemonLDAP::NG portal when default location-rule is accept", func() {
+		host := "default-accept.example.com"
+
+		bi := buildIngress(host, f.Namespace.Name)
+		bi.Annotations["kubernetes-controller.lemonldap-ng.org/location-rules"] = `{"default": "accept"}`
+
+		ing, err := f.EnsureIngress(bi)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(ing).NotTo(BeNil())
+
+		err = f.WaitForNginxServer(host,
+			func(server string) bool {
+				return Expect(server).Should(ContainSubstring("server_name default-accept.example.com")) &&
+					Expect(server).ShouldNot(ContainSubstring("return 503"))
+			})
+		Expect(err).NotTo(HaveOccurred())
+
+		time.Sleep(1 * time.Second) // FIXME wait for LLNG config reload
+
+		resp, _, errs := gorequest.New().
+			Get(f.NginxHTTPURL).
+			RedirectPolicy(func(req gorequest.Request, via []gorequest.Request) error { return http.ErrUseLastResponse }).
+			Set("Host", host).
+			End()
+
+		if len(errs) > 0 {
+			Expect(errs[0]).NotTo(HaveOccurred())
+		}
+		Expect(resp.StatusCode).Should(Equal(http.StatusFound))
+		expectedLocationHeader := "http://auth.example.org/?url=" + base64.StdEncoding.EncodeToString([]byte("http://default-accept.example.com/"))
+		Expect(resp.Header.Get("Location")).Should(Equal(expectedLocationHeader))
+	})
+
+	It("should return status code 200 when default location-rule is skip", func() {
+		host := "default-skip.example.com"
+
+		bi := buildIngress(host, f.Namespace.Name)
+		bi.Annotations["kubernetes-controller.lemonldap-ng.org/location-rules"] = `{"default": "skip"}`
+
+		ing, err := f.EnsureIngress(bi)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(ing).NotTo(BeNil())
+
+		err = f.WaitForNginxServer(host,
+			func(server string) bool {
+				return Expect(server).Should(ContainSubstring("server_name default-skip.example.com")) &&
+					Expect(server).ShouldNot(ContainSubstring("return 503"))
+			})
+		Expect(err).NotTo(HaveOccurred())
+
+		time.Sleep(1 * time.Second) // FIXME wait for LLNG config reload
+
+		resp, _, errs := gorequest.New().
+			Get(f.NginxHTTPURL).
+			RedirectPolicy(func(req gorequest.Request, via []gorequest.Request) error { return http.ErrUseLastResponse }).
+			Set("Host", host).
+			End()
+
+		if len(errs) > 0 {
+			Expect(errs[0]).NotTo(HaveOccurred())
+		}
+		Expect(resp.StatusCode).Should(Equal(http.StatusOK))
+		Expect(resp.Header.Get("Location")).Should(Equal(""))
+	})
+})
---
+++ test/e2e/settings/build_ingress.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2018 Mathieu Parent <math.parent@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package setting
+
+import (
+	v1beta1 "k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func buildIngress(host, namespace string) *v1beta1.Ingress {
+	return &v1beta1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        host,
+			Namespace:   namespace,
+			Annotations: map[string]string{},
+		},
+		Spec: v1beta1.IngressSpec{
+			Rules: []v1beta1.IngressRule{
+				{
+					Host: host,
+					IngressRuleValue: v1beta1.IngressRuleValue{
+						HTTP: &v1beta1.HTTPIngressRuleValue{
+							Paths: []v1beta1.HTTPIngressPath{
+								{
+									Path: "/",
+									Backend: v1beta1.IngressBackend{
+										ServiceName: "http-svc",
+										ServicePort: intstr.FromInt(80),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
